784636d8b07eee7ca55cb3346adca0da29922484
Gerardo Siebels <gerardo@Gerardos-iMac.local>
2019-12-03 13:19:53 +0000

    fist commit

---
+++ gs-gatsby-portfolio/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <a className="label">Created by Gerardo Siebels © 2019</a>
+    </footer>
+  )
+}
+
+export default Footer
